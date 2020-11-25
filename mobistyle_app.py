87a2d7d7214f39e3c3ef7e6473c070b4c7605993
SVGoogle <sandijs.vasilevskis@gmail.com>
2020-11-25 23:33:59 +0100

    docs: Create mobistyle-app.py file

--- mobistyle_app.py
+++ mobistyle_app.py
@@ -13,19 +13,33 @@ def main():
 
     # Sidebar layout
     st.sidebar.header('Visualization Features')
-    room_name = st.sidebar.selectbox('Select Room', ('1', '2', '3', '4', '5', '6', '7', '8'))
+    room_name = st.sidebar.selectbox('Select Room', ('Room 1', 'Room 2', 'Room 3', 'Room 4',
+                                                     'Room 5', 'Room 6', 'Room 7', 'Room 8'))
 
-    st.sidebar.subheader('About project')
-    st.sidebar.write("""
+    about_project = st.sidebar.beta_expander('About project')
+    about_project.write("""
     - Author: Sandijs Vasilevskis    
     - Project: MOBISTYLE, EU H2020 
+    - Website: [LINK](https://www.mobistyle-project.eu)
     - Period: 2018-2020
     - Deliverable: D6.2
-    - Project website: [LINK](https://www.mobistyle-project.eu)
     """)
+
+
+    expander = st.beta_expander("Room description")
+    expander.write("Here you could put in some really, really long explanations...")
 
     if st.button('Correlation Heatmap'):
         pass
+    st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+    with st.beta_expander("See explanation"):
+        st.write("""
+    ...         The chart above shows some numbers I picked for you.
+    ...         I rolled actual dice for these, so they're *guaranteed* to
+    ...         be random.
+    ...     """)
+        st.image("https://static.streamlit.io/examples/dice.jpg")
 
 
 if __name__ == '__main__':
